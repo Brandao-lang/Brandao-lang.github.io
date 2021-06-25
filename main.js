6315310e922811c5b3fc087c190babd921c16aea
Brandao-lang <elijahbrandao@gmail.com>
2021-06-25 11:39:26 -0400

    added video

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
+// let btn = document.querySelector('button')
+// btn.onclick(function (e) {
+//     window.location.assign('mailto:foo@bar.com')
+// })
 
+var slideIndex = 1;
+showSlides(slideIndex);
 
-let btn = document.querySelector('button')
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-btn.onclick(function (e) {
-    window.location.assign('mailto:foo@bar.com')
-})
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+ slides[slideIndex-1].style.display = "block";
+  
+}
 
 
 
